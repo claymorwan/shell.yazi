342ad80cb94556857dd1651a18429c4555ef45d6
claymorwan <claymorwan@fembois.dev>
2025-05-22 18:36:02 +0000

    feat: Added possibility to not skip block

--- README.md
+++ README.md
@@ -6,16 +6,17 @@ For now only works with [Bash](https://www.gnu.org/software/bash/), [Zsh](https:
 
 # Installation
 ## Install plugin
-```
+```bash
 ya pack -a 'claymorwan/shell'
 ```
 ## Setup keymaps
 Add this to your `keymaps.toml` file:
-```
+```toml
 prepend_keymap = [
 	# Shell plugin
-	{ on = [ ":" ], run = "plugin shell block", 	desc = "Run a shell command with your shell (block until finishes)" },
+	{ on = [ ":" ], run = "plugin shell block", desc = "Run a shell command with your shell (block until finishes)" },
 	{ on = [ ";" ], run = "plugin shell noblock", desc = "Run a shell command with your shell" },
+	{ on = [ "!" ], run = "plugin shell noskip", desc = "Run a shell command with your shell (block until finishes and when return key is pressed)" },
 ]
 ```
 
@@ -27,7 +28,7 @@ Works like the usual shel block command:
 ## Skipping stuff in your shell config file
 If you have stuff that runs when loading your shell (like a neofetch for example) you probably don't want it to run when using this plugin.
 You can put these commands in a condition with the `YAZI_SHELL_SKIP_CMD` variable like so:
-```
+```bash
 if [[ "$YAZI_SHELL_SKIP_CMD" != true ]]; then
   # Place commands here, for example:
   fastfetch
--- main.lua
+++ main.lua
@@ -1,31 +1,36 @@
 -- Makes the box and run the command
-function run_shell(isblock)
-  local value, event = ya.input({
-    title = "Shell" .. (isblock and " (block)" or ""),
-    position = { "top-center", y = 3, w = 40 },
-  })
+local function run_shell(isblock, noskip)
+	local value, event = ya.input({
+		title = "Shell" .. (isblock and (noskip and " (block no skip)" or " (block)") or ""),
+		position = { "top-center", y = 3, w = 40 },
+	})
 
-  if event == 1 then
-    ya.manager_emit("shell", {
-      "YAZI_SHELL_SKIP_CMD=true $SHELL -ic " .. ya.quote(value .. "; exit", true),
-      block = isblock,
-      confirm = true,
-    })
-  end
+	local noskip_cmd = (noskip and "echo -en '\nPress [Enter] to continue'; read;" or "")
+
+	if event == 1 then
+		ya.manager_emit("shell", {
+			"YAZI_SHELL_SKIP_CMD=true $SHELL -ic " .. ya.quote(value .. ";" .. noskip_cmd .. "exit", true),
+			block = isblock,
+			confirm = true,
+		})
+	end
 end
 
 return {
-  entry = function(self, jobs)
-    local action = jobs.args[1]
-        if not action then
-          return
-        end 
-    -- Block
-    if action == "block" then
-      run_shell(true)
-    -- No block
-    elseif action == "noblock" then
-      run_shell(false)
-    end
-  end,
+	entry = function(self, jobs)
+		local action = jobs.args[1]
+		if not action then
+			return
+		end
+		-- Block
+		if action == "block" then
+			run_shell(true, false)
+			-- No block
+		elseif action == "noblock" then
+			run_shell(false, false)
+			-- Block, no skip
+		elseif action == "noskip" then
+			run_shell(true, true)
+		end
+	end,
 }
